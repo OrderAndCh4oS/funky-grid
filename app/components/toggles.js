cba0d62fbc1592008046e70b798cac7ff6193e17
sean-kl <sean@kingslandlinassi.com>
2017-09-05 11:43:14 +0100

    Added toggle component and active class for filters

--- app/components/toggles.js
+++ app/components/toggles.js
@@ -3,16 +3,51 @@ Vue.component('toggles', {
     template: `
         <div class="toggles">
             <template v-for="category in categories">
-                <button @click="$emit('filter-category', category)">{{category | capitalise}}</button>&nbsp;
+                <toggle @filter-category="filterCategory" :category="category" :selected="category == 'all'"></toggle>&nbsp;
             </template>
             <button @click="$emit('toggle-grid')">{{ gridtoggle }}</button>
         </div>
     `,
+    data() {
+        return {
+            active: 'all'
+        }
+    },
+    methods: {
+        filterCategory(cat) {
+            this.$children.forEach(
+                toggle => {
+                    toggle.isActive = (toggle.category === cat);
+                }
+            );
+            this.$emit('filter-category', cat);
+        }
+    }
+});
+
+Vue.component('toggle', {
+    props: {
+        'category': {
+            'required': true
+        },
+        'selected': {
+            'default': false
+        }
+    },
+    template: `<button @click="$emit('filter-category', category)" name="category" :class="{'active':this.isActive}" >{{category | capitalise}}</button>`,
+    data() {
+        return {
+            isActive: false
+        }
+    },
+    mounted() {
+        this.isActive = this.selected;
+    },
     filters: {
         capitalise: function (value) {
-            if (!value) return ''
-            value = value.toString()
+            if (!value) return '';
+            value = value.toString();
             return value.charAt(0).toUpperCase() + value.slice(1)
         }
     }
-})
+});
